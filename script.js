afd4a17d41029d0b8ebc51a2997bee8e39e3fd12
Felipi7 <felipisabinocota@gmail.com>
2024-09-21 17:34:35 -0300

    Update

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const apiKey = "ff41766cb12a37d50eb6c74f0c924222"; 
+const apiKey = "ff41766cb12a37d50eb6c74f0c924222";
 
 function getCity() {
-    const city = document.querySelector(".input").value;
-    apiData(city); 
+    const city = document.querySelector(".input").value.trim();
+    if (city) {
+        apiData(city);
+    } else {
+        alert("Por favor, insira o nome de uma cidade.");
+    }
 }
 
 async function apiData(city) {
-    
+    try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`);
-        const data = await response.json(); 
-        Status(data)
+        if (!response.ok) {
+            throw new Error("Cidade não encontrada");
+        }
+        const data = await response.json();
+        updateWeatherInfo(data);
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
-function Status(data) {
+function updateWeatherInfo(data) {
     console.log(data);
-    document.querySelector(".city").innerHTML = "Temperatura " + data.name;
-    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + " ºC"; 
-    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + " ºC"; 
-    document.querySelector(".cloud").src =`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-    document.querySelector(".forecast").innerHTML = data.weather[0].description; 
-    document.querySelector(".humidity").innerHTML ="Umidade " + data.main.humidity + "%"  
-
+    const cityElement = document.querySelector(".city");
+    cityElement.innerHTML = data.name;
+    cityElement.classList.add("cidade-buscada"); 
+    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + " ºC";
+    document.querySelector(".cloud").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    document.querySelector(".forecast").innerHTML = data.weather[0].description;
+    document.querySelector(".humidity").innerHTML = "Umidade " + data.main.humidity + "%";
 }
-
