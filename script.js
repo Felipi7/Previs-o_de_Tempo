129ab029bde63c49a167da2baa6726a5c3b7208a
Felipi7 <felipisabinocota@gmail.com>
2024-09-23 18:11:05 -0300

    Updates

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const apiKey = "ff41766cb12a37d50eb6c74f0c924222";
 
 function getCity() {
-    const city = document.querySelector(".input").value.trim();
-    if (city) {
-        apiData(city);
+    const cityInput = document.querySelector(".input").value.trim();
+    if (cityInput) {
+        apiData(cityInput);
     } else {
         alert("Por favor, insira o nome de uma cidade.");
     }
@@ -23,13 +23,26 @@ async function apiData(city) {
 }
 
 function updateWeatherInfo(data) {
-    console.log(data);
     const cityElement = document.querySelector(".city");
+    const temperatureElement = document.querySelector(".temperature");
+    const cloudElement = document.querySelector(".cloud");
+    const forecastElement = document.querySelector(".forecast");
+    const humidityElement = document.querySelector(".humidity");
+    const countryElement = document.querySelector(".country");
+
     cityElement.innerHTML = data.name;
-    cityElement.classList.add("cidade-buscada"); 
-    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + " ºC";
-    document.querySelector(".cloud").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    document.querySelector(".forecast").innerHTML = data.weather[0].description;
-    document.querySelector(".humidity").innerHTML = "Umidade " + data.main.humidity + "%";
-    
+    cityElement.classList.add("cidade-buscada");
+    temperatureElement.innerHTML = Math.floor(data.main.temp) + " ºC";
+    cloudElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    forecastElement.innerHTML = data.weather[0].description;
+    humidityElement.innerHTML = "Umidade: " + data.main.humidity + "%";
+    countryElement.innerHTML = "País: " + data.sys.country;
 }
+
+function hours() {
+    const HoraAtual = new Date();
+    const horaFormatada = HoraAtual.toLocaleTimeString();
+    document.querySelector(".hora").innerHTML = horaFormatada;
+}
+
+setInterval(hours, 1000);
